19bea34e9b08d235c60b86e3d5d601add05e1bce
simmetric <github@simmetric.nl>
2017-05-08 12:42:38 +0200

    Fixed unicode support

--- SpacedText/Interop.cs
+++ SpacedText/Interop.cs
@@ -11,25 +11,25 @@ namespace SpacedTextPlugin
 
     internal class Interop
     {
-        [DllImport("gdi32.dll", CharSet = CharSet.Auto)]
+        [DllImport("gdi32.dll", CharSet = CharSet.Unicode)]
         public static extern int SetTextCharacterExtra(IntPtr hdc, int nCharExtra);
 
-        [DllImport("gdi32")]
+        [DllImport("gdi32", CharSet = CharSet.Unicode)]
         public static extern bool TextOut(IntPtr hdc, int x, int y, string textstring, int charCount);
         
-        [DllImport("gdi32.dll", EntryPoint = "SelectObject")]
+        [DllImport("gdi32.dll", CharSet = CharSet.Unicode, EntryPoint = "SelectObject")]
         public static extern IntPtr SelectObject([In] IntPtr hdc, [In] IntPtr hgdiobj);
 
-        [DllImport("gdi32")]
+        [DllImport("gdi32", CharSet = CharSet.Unicode)]
         public static extern bool DeleteObject(IntPtr objectHandle);
 
-        [DllImport("gdi32")]
+        [DllImport("gdi32", CharSet = CharSet.Unicode)]
         public static extern uint SetTextColor(IntPtr hdc, int crColor);
 
-        [DllImport("gdi32")]
+        [DllImport("gdi32", CharSet = CharSet.Unicode)]
         public static extern uint SetBkColor(IntPtr hdc, int crColor);
 
-        [DllImport("gdi32")]
+        [DllImport("gdi32", CharSet = CharSet.Unicode)]
         public static extern void GetTextExtentPoint(IntPtr hdc, string text, int charcount, ref Size size);
     }
 }
